59c1b910ab8bca78400d2647183a3dcf6e665cf8
Grandson <achiunjikanjikho@gmail.com>
2024-12-30 17:57:29 +0200

    Update of my homepage and portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './templates/**/*.html', // Adjust path based on your project structure
+    './**/templates/**/*.html',
+    './static/src/**/*.js', // Optional, for any scripts
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
